302b7fddeb47fbfa753090d902a607f29ba2e268
etienne van Leeuwen <etiennesoccerpro@gmail.com>
2026-06-30 15:17:40 +0200

    tm 9 af + counter en weather weg

--- Blazoropdracht/data/Reservation.cs
+++ Blazoropdracht/data/Reservation.cs
@@ -1,6 +1,7 @@
-﻿using Blazoropdracht;
+﻿using System.ComponentModel.DataAnnotations;
+using Blazoropdracht;
 
-public class Reservation
+public class Reservation : IValidatableObject
 {
     public int Id { get; set; }
     public int PetId { get; set; }
@@ -9,4 +10,12 @@ public class Reservation
     public Cage? Cage { get; set; }
     public DateOnly StartDate { get; set; }
     public DateOnly EndDate { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (EndDate < StartDate)
+            yield return new ValidationResult(
+                "End date cannot be before start date.",
+                [nameof(EndDate)]);
+    }
 }
